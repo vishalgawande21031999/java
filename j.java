dd910818f73a9415af4e171fd8386024aafb4818
vishalgawande21031999 <48825418+vishalgawande21031999@users.noreply.github.com>
2019-08-04 14:10:07 +0530

    Add files via upload

---
+++ j.java
@@ -0,0 +1,27 @@
+class Main{ 
+    static void count(String str) 
+    { 
+         
+        char[] ch = str.toCharArray(); 
+        for (int i = 0; i < ch.length; i++) { 
+  
+    n 
+            String s = ""; 
+  
+            while (i < ch.length && ch[i] != ' ') { 
+  
+        
+                s = s + ch[i]; 
+                i++; 
+            } 
+  
+            if (s.length() > 0)  
+                System.out.println(s + "->" + s.length());             
+        } 
+    } 
+    public static void main(String[] args) 
+    { 
+        String str = "hello world s ds s"; 
+        count(str); 
+    } 
+} 
